340122aa4f2230f1acc1761fe1190b4c0301078e
dadaria <dashnaky@gmail.com>
2020-01-12 17:48:39 +0300

    lesso2 completed

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MessageField from './components/MessageField';
 
-let messages = ['Привет', 'Как дела?'];
+// let messages = ['Привет', 'Как дела?'];
 
-const handleClick = () => {
-   messages.push('Нормально');
-   ReactDOM.render(
-      <MessageField messages = { messages } />,
-      document.getElementById('root')
-   )
-}
+// const handleClick = () => {
+//    messages.push('Нормально');
+//    ReactDOM.render(
+//       <MessageField messages = { messages } />,
+//       document.getElementById('root')
+//    )
+// }
 
-const MessageComponent = (props) => <div>{ props.text }</div>;
+// const MessageComponent = (props) => <div>{ props.text }</div>;
 
-const MessageField = (props) => {
-   const messageElements = props.messages.map(message => <MessageComponent text={ message } />);
-   return (
-      <div>
-         <h1>React Chat</h1>
-         { messageElements }
-         <button onClick = { handleClick }>Отправить сообщение</button>
-      </div>      
-   );
-};
+// const MessageField = (props) => {
+//    const messageElements = props.messages.map(message => <MessageComponent text={ message } />);
+//    return (
+//       <div>
+//          <h1>React Chat</h1>
+//          { messageElements }
+//          <button onClick = { handleClick }>Отправить сообщение</button>
+//       </div>      
+//    );
+// };
 
 ReactDOM.render(
-   <MessageField messages = { messages } />,
+   <MessageField />,
    document.getElementById('root')
 )
