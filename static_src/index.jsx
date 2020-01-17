f74c4ed9582b1f608efbbd4f2fbb13eebbec9c10
dadaria <dashnaky@gmail.com>
2020-01-17 15:02:24 +0300

    adding redux

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Router from './components/Router';
+import initStore from './utils/store';
 
 
 ReactDOM.render(
-   <BrowserRouter>
-      <MuiThemeProvider>
-         <Router />
-      </MuiThemeProvider>
-   </BrowserRouter>
+   <Provider store={ initStore() }>
+      <BrowserRouter>
+         <MuiThemeProvider>
+            <Router />
+         </MuiThemeProvider>
+      </BrowserRouter>
+   </Provider>
 ,
    document.getElementById('root')
 )
